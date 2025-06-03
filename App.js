ae4674c4ad55fc755cb40f9e6213fb470e2b093b
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2025-06-03 20:11:34 -0300

    feat(mines): initial project setup with core config and entry files

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { View, Text, Plataform, StyleSheet } from 'react-native'
+import params from './src/params'
+
+export default class App extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>Iniciando o Mines!</Text>
+                <Text style={styles.instructions}>Tamanho da Grade:
+                    {params.getRowsAmount()}x{params.getColumnsAmount()}
+                </Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    }
+})
