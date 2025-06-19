493ab618160df9621eb579e6a67e86de7234941b
Kauê Lima <56000639+KaueACLima@users.noreply.github.com>
2025-06-18 21:23:03 -0300

    style(Mines): apply final styling adjustments

--- src/components/Mines.js
+++ src/components/Mines.js
@@ -15,6 +15,22 @@ export default props => {
 
 const style = StyleSheet.create({
     container: {
-        
+        alignItems: center,
+        justifyContent: center,
+    },
+    coreMine: {
+        height: 14,
+        width: 14,
+        borderRadius: 10,
+        backgroundColor: 'black',
+        alignItems: center,
+        justifyContent: center,
+    },
+    line: {
+        position: 'abolute',
+        height: 3,
+        width: 20,
+        borderRadius: 3,
+        backgroundColor: 'black',
     }
 })
